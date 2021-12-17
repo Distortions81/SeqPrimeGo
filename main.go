8365f226f7cda72f1044f2a66138c2b86f869a02
Distortions81 <33408758+Distortions81@users.noreply.github.com>
2021-12-16 23:22:25 -0700

    goroutine

--- main.go
+++ main.go
@@ -4,34 +4,56 @@ import (
 	"fmt"
 	"math/big"
 	"runtime"
+	"strconv"
 
 	"github.com/remeh/sizedwaitgroup"
 )
 
 func main() {
-	x := big.NewInt(0)
+	var x int64 = 0
 
-	swg := sizedwaitgroup.New(runtime.NumCPU() * 2)
-	fmt.Println("Starting", runtime.NumCPU(), "threads...")
+	swg := sizedwaitgroup.New(runtime.NumCPU())
+	fmt.Println("Starting", runtime.NumCPU(), "threads.")
 
-	for x.SetString("1", 0); true; x.Add(x, big.NewInt(1)) {
+	for x = 1; x < 9223372036854775807; x++ {
 		swg.Add()
-		go func() {
-			buf := ""
-			y := big.NewInt(0)
-			for y.SetString("1", 0); y.Cmp(x) == -1; y.Add(y, big.NewInt(1)) {
-				buf = buf + y.String()
-			}
-			temp := big.NewInt(0)
-			temp.SetString(buf, 0)
+		go func(val int64) {
 
-			if temp.ProbablyPrime(0) {
-				fmt.Print(temp, ", ")
+			buf := ""
+			var y int64 = 0
+			// Count up
+			for y = 1; y < x; y++ {
+				buf = buf + strconv.FormatInt(y, 10)
+			}
+			//Count down
+			for y = y - 1; y > 0; y-- {
+				buf = buf + strconv.FormatInt(y, 10)
+			}
+
+			temp := big.NewInt(0)
+			temp.SetString(buf, 10)
+
+			if isPrime(temp) {
+				fmt.Print(val, ", ")
+
 			} else {
-				//fmt.Print("NOT: ", temp, ", ")
+				//fmt.Print("!N=", val, ", ")
 			}
 			swg.Done()
-		}()
-		swg.Wait()
+		}(x)
 	}
+	swg.Wait()
+}
+
+func isPrime(num *big.Int) bool {
+	i := big.NewInt(0)
+	iSq := big.NewInt(0)
+	iSq = iSq.Sqrt(num)
+
+	for i.SetString("2", 10); i.Cmp(iSq) == -1; i.Add(i, big.NewInt(1)) {
+		if num.Mod(num, i) == big.NewInt(0) {
+			return false
+		}
+	}
+	return true
 }
