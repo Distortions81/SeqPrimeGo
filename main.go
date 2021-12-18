1c4f5db46575c919455152b7dfec63e35172e3fd
Distortions81 <33408758+Distortions81@users.noreply.github.com>
2021-12-18 02:57:51 -0700

    increase threads

--- main.go
+++ main.go
@@ -55,7 +55,7 @@ func main() {
 	log.Println("Checking for n=x primes: ")
 
 	//Wait group with cpu threds
-	threads := runtime.NumCPU()
+	threads := runtime.NumCPU() * 10
 	swg := sizedwaitgroup.New(threads)
 	log.Println("Starting", threads, "new threads.")
 
